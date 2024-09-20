90460fad8b00ebcbc41b23fbbb9cd70f0c1e9d8f
CEOFYEAST <benton.diebold@me.com>
2024-09-20 14:14:38 -0700

    redeployed

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/UserSystemPrototype/client/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/UserSystemPrototype/" : "/",
   transpileDependencies: true
 })
