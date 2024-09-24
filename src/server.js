78abfdd7cb11edd7851c5eba47856736bc34c333
CEOFYEAST <benton.diebold@me.com>
2024-09-24 12:59:21 -0700

    whyyyyy

--- src/server.js
+++ src/server.js
@@ -1,31 +1,59 @@
-const fastify = require('fastify')({logger: true})
+// const fastify = require('fastify')({logger: true})
 
-fastify.register(require('@fastify/cors'), {})
-fastify.register(require('@fastify/swagger'), {})
-fastify.register(require('@fastify/swagger-ui'), {
-    routePrefix: '/docs',
-    uiConfig: {
-        docExpansion: 'full',
-        deepLinking: false
-    },
-    uiHooks: {
-        onRequest: function (request, reply, next) { next() },
-        preHandler: function (request, reply, next) { next() }
-    },
-    staticCSP: true,
-    transformStaticCSP: (header) => header,
-    transformSpecification: (swaggerObject, request, reply) => { return swaggerObject },
-    transformSpecificationClone: true
+// fastify.register(require('@fastify/cors'), {})
+// fastify.register(require('@fastify/swagger'), {})
+// fastify.register(require('@fastify/swagger-ui'), {
+//     routePrefix: '/docs',
+//     uiConfig: {
+//         docExpansion: 'full',
+//         deepLinking: false
+//     },
+//     uiHooks: {
+//         onRequest: function (request, reply, next) { next() },
+//         preHandler: function (request, reply, next) { next() }
+//     },
+//     staticCSP: true,
+//     transformStaticCSP: (header) => header,
+//     transformSpecification: (swaggerObject, request, reply) => { return swaggerObject },
+//     transformSpecificationClone: true
+// })
+// fastify.register(require('./routes/items.js'))
+
+// const start = async () => {
+//     try {
+//         await fastify.listen(process.env.PORT || 5001)
+//     } catch(error) {
+//         fastify.log.error(error)
+//         process.exit(1)
+//     }
+// }
+
+// start()
+
+
+'use strict'
+
+// Read the .env file.
+require('dotenv').config()
+
+// Require the framework
+const Fastify = require('fastify')
+
+// Instantiate Fastify with some config
+const app = Fastify({
+  logger: true,
+  pluginTimeout: 10000
 })
-fastify.register(require('./routes/items.js'))
 
-const start = async () => {
-    try {
-        await fastify.listen(process.env.PORT || 5001)
-    } catch(error) {
-        fastify.log.error(error)
-        process.exit(1)
-    }
-}
+app.register(require('@fastify/cors'), {})
+app.register(require('@fastify/swagger'), {})
+app.register(require('@fastify/swagger-ui'), {})
+app.register(require('./routes/items.js'))
 
-start()
+// Start listening.
+app.listen(process.env.PORT || 3000, '0.0.0.0', (err) => {
+  if (err) {
+    app.log.error(err)
+    process.exit(1)
+  }
+})
